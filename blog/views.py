cbdc49fd7deff9b828ffd929619f3e9c27b0a933
Юрий <mulch@list.ru>
2023-04-04 15:03:16 +0300

    optimize comments count evaluation.

--- blog/views.py
+++ blog/views.py
@@ -8,8 +8,7 @@ def serialize_post_optimized(post):
         'title': post.title,
         'teaser_text': post.text[:200],
         'author': post.author.username,
-        # 'comments_amount': len(Comment.objects.filter(post=post)),
-        'comments_amount': post.comments_amount,
+        'comments_amount': post.comments_count,
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
@@ -18,36 +17,38 @@ def serialize_post_optimized(post):
     }
 
 
-# def serialize_post(post):
-#     return {
-#         'title': post.title,
-#         'teaser_text': post.text[:200],
-#         'author': post.author.username,
-#         'comments_amount': len(Comment.objects.filter(post=post)),
-#         'image_url': post.image.url if post.image else None,
-#         'published_at': post.published_at,
-#         'slug': post.slug,
-#         'tags': [serialize_tag(tag) for tag in post.tags.all()],
-#         'first_tag_title': post.tags.all()[0].title,
-#     }
-
-
 def serialize_tag(tag):
     return {
         'title': tag.title,
-        'posts_with_tag': len(Post.objects.filter(tags=tag)),
+        'posts_with_tag': Post.objects.filter(tags=tag).count(),
     }
 
 
 def index(request):
-    most_popular_posts = Post.objects.prefetch_related('author')\
-                             .annotate(count_likes=Count('likes', distinct=True),
-                                       comments_amount=Count('comments', distinct=True))\
-                             .order_by('-count_likes')[:5]
-    most_fresh_posts = Post.objects.prefetch_related('author')\
-                           .annotate(comments_amount=Count('comments', distinct=True))\
-        .order_by('-published_at')[:5]
+    # most_popular_posts = Post.objects.prefetch_related('author')\
+    #                          .annotate(count_likes=Count('likes', distinct=True),
+    #                                    comments_amount=Count('comments', distinct=True))\
+    #                          .order_by('-count_likes')[:5]
+    most_popular_posts = Post.objects.annotate(count_likes=Count('likes')).order_by('-count_likes')[:5]
+    most_popular_posts_ids = [post.id for post in most_popular_posts]
+    posts_with_comments = Post.objects.filter(id__in=most_popular_posts_ids).annotate(comments_count=Count('comments'))
+    ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
+    count_for_id = dict(ids_and_comments)
+    print(count_for_id)
+    for post in most_popular_posts:
+        post.comments_count = count_for_id[post.id]
+
+    most_fresh_posts = Post.objects.annotate(comments_amount=Count('comments')).order_by('-published_at')[:5]
+    most_fresh_posts_ids = [post.id for post in most_fresh_posts]
+    posts_with_comments = Post.objects.filter(id__in=most_fresh_posts_ids).annotate(comments_count=Count('comments'))
+    ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
+    count_for_id = dict(ids_and_comments)
+    print(count_for_id)
+    for post in most_fresh_posts:
+        post.comments_count = count_for_id[post.id]
+
     most_popular_tags = Tag.objects.annotate(count_tags=Count('posts')).order_by('-count_tags')[:5]
+
     context = {
         'most_popular_posts': [serialize_post_optimized(post) for post in most_popular_posts],
         'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts],
@@ -67,7 +68,7 @@ def post_detail(request, slug):
             'author': comment.author.username,
         })
 
-    likes = post.likes.all()
+    likes_count = post.likes.count()
     related_tags = post.tags.all()
 
     serialized_post = {
@@ -75,7 +76,7 @@ def post_detail(request, slug):
         'text': post.text,
         'author': post.author.username,
         'comments': serialized_comments,
-        'likes_amount': len(likes),
+        'likes_amount': likes_count,
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
